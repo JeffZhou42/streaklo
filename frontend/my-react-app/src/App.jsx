484a03a2f87d9457e0795bdb8ed35a9fa8204329
Zhaoyyyyyyyy <rayzhao05@gmail.com>
2024-09-28 17:57:38 -0400

    name is changeable

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import './App.css';
-import PhotoCarousel from './PhotoCarousel';
-import Habits from './Habits';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
+import PhotoCarousel from "./PhotoCarousel";
+import Habits from "./Habits";
+import axios from "axios";
 
 function App() {
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/getUser")
+      .then((result) => {
+        setUser(result.data), location.update();
+      })
+      .catch((err) => console.log(err));
+  });
+
   const photos = [
-    '/photo1.jpg',
-    '/photo2.jpg',
-    '/photo3.jpg',
-    '/photo4.jpg',
-    '/photo5.jpg'
+    "/photo1.jpg",
+    "/photo2.jpg",
+    "/photo3.jpg",
+    "/photo4.jpg",
+    "/photo5.jpg",
   ];
 
   return (
@@ -20,21 +32,28 @@ function App() {
           <h1 className="streaklo-text">Streaklo</h1>
         </div>
         <Routes>
-          <Route path="/" element={
-            <>
-              <header className="greeting-header">
-                <div className="greeting-wrapper">
-                  <h1 className="greeting">hi, john. what's today going to look like?</h1>
-                </div>
-              </header>
-              <h2 className="recap-header">catch up</h2>
-              <PhotoCarousel photos={photos} />
-              <Link to="/habits" className="habits-link">
-                <span className="plus-symbol">+</span>
-                <span className="habits-text">Habits</span>
-              </Link>
-            </>
-          } />
+          <Route
+            path="/"
+            element={
+              <>
+                <header className="greeting-header">
+                  <div className="greeting-wrapper">
+                    <h1 className="greeting">
+                      {"hi," +
+                        user[0].name +
+                        " what's today going to look like?"}
+                    </h1>
+                  </div>
+                </header>
+                <h2 className="recap-header">catch up</h2>
+                <PhotoCarousel photos={photos} />
+                <Link to="/habits" className="habits-link">
+                  <span className="plus-symbol">+</span>
+                  <span className="habits-text">Habits</span>
+                </Link>
+              </>
+            }
+          />
           <Route path="/habits" element={<Habits />} />
         </Routes>
       </div>
