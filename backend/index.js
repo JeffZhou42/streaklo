c47da545b376b08fbf0d0490b56c3c00e43856ea
JeffZhou42 <purpleshep42yee@gmail.com>
2024-09-28 12:55:22 -0400

    figured out how to make habits work

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,12 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const UserModel = require("./models/User")
 const HabitModel = require("./models/Habit")
+const FriendsModel = require("./models/Friends")
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 mongoose.connect("mongodb://localhost:27017/test")
+
+app.get("/getHabit")
--- backend/models/Habit.js
+++ backend/models/Habit.js
@@ -2,12 +2,17 @@ const mongoose = require("mongoose")
 
 const HabitSchema = new mongoose.Schema({
     habitName: String,
-    habitImages: [{
+    //streaks stored, personal streaks and friend streaks (combined)
+    personalStreak: Number,
+    imageList: [{
         dailyImage: binData
     }],
-    streaks: Number
-})
-//keep the images stored till someone's streak is broken
-//set their streak to -1 when it's broken 
+    //for demo, add images to preset daily image list
+    //just have user break their own streak to clear image list
+    people: [{
+        name: String,
+        dailyStreak: Number,
+    }]
+}) 
 const HabitModel = mongoose.model("habits", HabitSchema)
 module.exports = HabitModel
